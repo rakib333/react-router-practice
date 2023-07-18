f9b94ae7ceb6e78ede6c30e6f7601694df18a86e
rakib333 <smrakib333@gmail.com>
2023-07-18 11:10:56 +0600

    navlink added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,16 @@
 // import React from 'react';
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import './Header.css'
+import ActiveLink from "../ActiveLink/ActiveLink";
 
 const Header = () => {
     return (
         <div className="navbar">
-            <Link to="/">Home</Link>
-            <Link to="/about">About</Link>
-            <Link to="/contact">Contact</Link>
-            <Link to="/posts">Posts</Link>
-            <Link to="/friends">Friends</Link>
+            <ActiveLink to="/">Home</ActiveLink>
+            <ActiveLink to="/about">About</ActiveLink>
+            <ActiveLink to="/contact">Contact</ActiveLink>
+            <ActiveLink to="/friends">Friends</ActiveLink>
+            <ActiveLink to="/posts">Posts</ActiveLink>
         </div>
     );
 };
